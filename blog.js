26b760d6909c1dc2d5a0b43add0e7f05a2c7d286
NurFitriani41 <nur.fitri@upi.edu>
2024-05-09 19:03:09 +0700

    Ini file JavaScript

---
+++ blog.js
@@ -0,0 +1,46 @@
+let articles = {
+    rekreasi: [
+        { title: 'Wahana Air Terbesar di Jawa Barat "Wahoo Waterworld"', content: 'Wahoo Waterworld adalah salah satu destinasi wisata air terbesar di Jawa Barat. Tempat wisata ini terletak di Jalan Pancatengah Parahyangan St no. 1, Kelurahan/Desa Jaya Mekar, Kecamatan Padalarang, Kabupaten Bandung Barat, Provinsi Jawa Barat.<br><br>Wahoo Waterworld terletak di pinggir jalan raya, sehingga mudah diakses dengan kendaraan pribadi maupun transportasi umum. Jika menggunakan kendaraan pribadi, wisatawan dapat menempuh perjalanan dari pusat kota Bandung menuju Wahoo Waterworld dengan waktu sekitar 30 menit.Jika menggunakan transportasi umum, wisatawan dapat naik angkutan kota (angkot) jurusan Bandung-Padalarang. Turun di depan Wahoo Waterworld.<br><br>Wahoo Waterworld sangat populer sebagai waterpark terbesar di Jawa Barat, serta memiliki wahana seluncuran tercepat di Asia Tenggara. Setiap kolam renang dilengkapi dengan wahana air yang akan memanjakan para pengunjung. Bagi kolam renang dewasa dilengkapi dengan wahana yang akan menguji adrenalin.<br><br>Selain itu, kondisi keseluruhan Wahoo Waterworld masih terjaga kebersihan, dan keindahannya. setelah selesai menjajal semua wahana yang ada di Wahoo Waterworld, nggak ada salahnya nogkrong keren dulu di objek wisata Tepi Danau ya.<br><br>Harga tiket masuk Wahoo Waterworld adalah Rp100.000 per orang untuk hari biasa dan Rp120.000 per orang untuk akhir pekan dan hari libur.', image: 'assets/wahoo.jpeg' },
+        { title: 'Meluncur di Water Coaster Dusun Bambu', content: 'Sesuai namanya, Water Coaster berada di dalam kawasan restoran Dusun Bambu yang telah dikenal dengan masakan lezat dan suasananya yang nyaman. Lokasinya sendiri berada di Jl. Kolonel Masturi km 11, Kabupaten Bandung Barat tak jauh dari kawasan Lembang yang terkenal.<br><br>Selain restoran, ternyata Dusun Bambu juga mengembangkan destinasi wisata berupa taman bermain untuk semua usia yang diberi nama We Playground. Nah, Water Coaster Dusun Bambu ini merupakan bagian dari We Playground tersebut.<br><br>Dusun Bambu buka setiap hari mulai dari pukul 10 pagi hingga 8 malam. Namun, untuk aktivitas seperti We Playground dan Water coaster hanya beroperasi hingga pukul 5 sore aja ya, sob.<br><br>Untuk merasakan sensasi meluncur sepanjang 400 meter ini sobat harus membeli tiket sebesar Rp60.000 per orang. Tiket ini sudah termasuk jas hujan sekali pakai sebagai pelindung dari cipratan air. Jadi nggak perlu khawatir pulang dengan kondisi basah kuyup, sob. Jika sobat TRAC ingin melanjutkan permainan di We Playground maka bisa menikmati taman bermain ini dengan tiket seharga Rp75.000.<br><br>Terdapat beberapa aturan yang harus dipatuhi jika Sobat TRAC ingin merasakan serunya Water Coaster Dusun Bambu. Wahana ini tidak diperuntukan bagi anak-anak dengan tinggi di bawah 100 cm, serta memiliki berat badan lebih dari 85 kg.<br><br>Sobat akan meluncur dengan menggunakan semacam ban khusus seperti yang biasa ditemukan di waterpark dengan kapasitas 2 dan 3 orang. Jadi jika datang sendiri maka bukan tak mungkin akan digabung dengan orang lain saat meluncur. Tapi, mana seru jika sendirian! Jangan lupa juga menjaga barang berharga dari cipratan air, seperti handphone dan benda elektronik lainnya.<br><br>Seperti yang sudah disampaikan, di sini terdapat beberapa wahana lain yang tak kalah seru dari Water Coaster. Jika kurang puas meluncur di Water Coaster ada pula Boomerang Rainbow Slide yang berupa perosotan raksasa dengan lintasan berwarna-warni.', image: 'assets/dusun-bambu.png' },
+        { title: 'Nimo Eye, Bianglala Elevasi Tertinggi di Indonesia', content: 'Nimo Eye menjadi daya tarik wisata baru di kawasan Bandung, Jawa Barat. Nimo Eye sendiri merupakan nama untuk wahana bianglala di Pangalengan. Bianglala yang diklaim memiliki elevasi tertinggi di Indonesia ini berada di kawasan wisata Nimo Highland Pangalengan. Berada di ketinggian 1400 mdpl, Nimo Eye memiliki sekitar 28 gondola. Menurut info di media sosial Nimo Eye, setiap gondola ini mampu menampung hingga empat orang wisatawan dewasa.<br><br>Dari atas bianglala ini, pengunjung akan disuguhkan panorama alam menakjubkan berupa hamparan luas perkebunan teh dengan barisan perbukitan di sekitarnya. Karena itu, sebelum naik Nimo Eye, pastikan untuk menyiapkan alat rekam gambar untuk mengabadikan view alam dari ketinggian yang memanjakan mata.<br><br>Nimo Eye memiliki dua jenis tiket yakni “Reguler” dan “Sunrise”. Untuk tiket reguler, pengunjung perlu membayar sebesar Rp 40.000 (weekdays) dan Rp 50.000 (weekend). Harga tersebut sudah termasuk tiket untuk satu orang, 1 lap (putaran bianglala) durasi 10 menit dan 1 cabin dengan cust.<br><br>Kemudian, untuk tiket “Sunrise” harganya sebesar Rp 50.000 yang sudah termasuk tiket untuk satu orang, 1 lap (putaran bianglala) durasi 10 menit dan 1 cabin dengan cust lain. Tiket sunrise ini hanya tersedia di hari Sabtu dan Minggu pada pukul 05.00 sampai 07.30. Kelebihan tiket ini yaitu kita bisa menyaksikan view matahari terbit yang sangat cantik dari atas bianglala.<br><br>Kemudian, untuk harga tiket masuk (HTM) Nimo Highlandnya sendiri cukup terjangkau yakni hanya sebesar Rp 35.000 (dewasa) dan Rp 25.000 (anak-anak).<br><br>Jika tertarik berkunjung,Nimo Highland berlokasi di daerah Banjarsari, Kecamatan Pangalengan, Kabupaten Bandung, Jawa Barat. Destinasi wisata ini buka setiap hari mulai pukul 12.00 sampai 18.00 WIB (weekdays) dan pukul 09.00 sampai 18.00 WIB (weekend).', image: 'assets/bianglala.jpg' }
+    ],
+    pantai: [
+        { title: 'Judul', content: 'Isi Artikel', image: 'https://example.com/borobudur.jpg' },
+        { title: 'Judul', content: 'Isi Artikel', image: 'https://example.com/borobudur.jpg' },
+        { title: 'Judul', content: 'Isi Artikel', image: 'https://example.com/kota-tua.jpg' }
+    ],
+    gunung: [
+        { title: 'Judul', content: 'Isi Artikel', image: 'https://example.com/borobudur.jpg' },
+        { title: 'Judul', content: 'Isi Artikel', image: 'https://example.com/kota-tua.jpg' },
+        { title: 'Judul', content: 'Isi Artikel', image: 'https://example.com/borobudur.jpg' },
+        { title: 'Judul', content: 'Isi Artikel', image: 'https://example.com/kota-tua.jpg' }
+    ]
+};
+
+function showArticles(category) {
+    let articleContent = document.getElementById('articleContent');
+    let articleImage = document.getElementById('articleImage');
+    articleContent.innerHTML = '';
+    articleImage.src = '';
+
+    let categoryList = document.getElementById(`${category}List`);
+    categoryList.innerHTML = '';
+
+    let articleData = articles[category];
+    if (articleData) {
+        articleData.forEach(article => {
+            let listItem = document.createElement('li');
+            let link = document.createElement('a');
+            link.href = '#';
+            link.textContent = article.title;
+            link.onclick = function() {
+                articleContent.innerHTML = `<center><h3>${article.title}</h3></center><br><p>${article.content}</p>`;
+                articleImage.src = article.image;
+            };
+            listItem.appendChild(link);
+            categoryList.appendChild(listItem);
+        });
+    } else {
+        categoryList.innerHTML = '<p>Tidak ada artikel dalam kategori ini.</p>';
+    }
+}
